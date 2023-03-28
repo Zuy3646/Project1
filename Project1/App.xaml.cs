600147e8842b15a2f4c97299c40920e910c75c65
Opleyha <google@mail.ru>
2023-03-28 10:15:27 +0300

    Добавьте файлы проекта.

---
+++ Project1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Project1
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new MainWindow().Show();    
+        }
+    }
+}
---
+++ Project1/View/Pages/PageMain.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Project1.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnGreate_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.ConectFrame.Navigate(new PageCreate());
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.ConectFrame.Navigate(new PagReg());
+        }
+    }
+}
